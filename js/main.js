5632fc42e5ef163e6ed5c299b07e8d2ef3702b47
Dan Schapira <dsschapira@gmail.com>
2018-06-25 15:19:56 -0400

    feat - updating styles, tags, and roles for accessibility

--- js/main.js
+++ js/main.js
@@ -164,7 +164,7 @@ createRestaurantHTML = (restaurant) => {
   image.alt = ""; 
   li.append(image);
 
-  const name = document.createElement('h1');
+  const name = document.createElement('h2');
   name.innerHTML = restaurant.name;
   li.append(name);
 
@@ -178,6 +178,7 @@ createRestaurantHTML = (restaurant) => {
 
   const more = document.createElement('a');
   more.innerHTML = 'View Details';
+  more.setAttribute('role','button');
   more.href = DBHelper.urlForRestaurant(restaurant);
   li.append(more)
 
