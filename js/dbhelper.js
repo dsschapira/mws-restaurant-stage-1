17ef86c24606086c7458bfa9adfe701094554cf1
Dan Schapira <dsschapira@gmail.com>
2018-08-14 11:51:31 -0400

    Use fetch api instead pf XmlHttpRequest

--- js/dbhelper.js
+++ js/dbhelper.js
@@ -16,18 +16,15 @@ class DBHelper {
    * Fetch all restaurants.
    */
   static fetchRestaurants(callback) {
-    let xhr = new XMLHttpRequest();
-    xhr.open('GET', DBHelper.DATABASE_URL);
-    xhr.onload = () => {
-      if (xhr.status === 200) { // Got a success response from server!
-        const restaurants = JSON.parse(xhr.responseText);
+    fetch(DBHelper.DATABASE_URL)
+      .then( response => response.json() )
+      .then( restaurants => {
         callback(null, restaurants);
-      } else { // Oops!. Got an error from server.
-        const error = (`Request failed. Returned status of ${xhr.status}`);
-        callback(error, null);
-      }
-    };
-    xhr.send();
+      })
+      .catch( error => {
+        const err = (`Request failed. Error returned: ${error}`);
+        callback(err, null);
+      })
   }
 
   /**
